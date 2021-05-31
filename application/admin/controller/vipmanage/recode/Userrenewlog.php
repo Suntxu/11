ad0876812c911b79a0372b6026f877febdf9c7a7
yanhui <1187371529@qq.com>
2021-05-31 18:20:34 +0800

    更改 域名续费记录 展示信息

--- application/admin/controller/vipmanage/recode/Userrenewlog.php
+++ application/admin/controller/vipmanage/recode/Userrenewlog.php
@@ -4,7 +4,7 @@ namespace app\admin\controller\vipmanage\recode;
 
 use app\common\controller\Backend;
 use think\Db;
-
+use app\admin\common\Fun;
 /**
  * 用户续费记录
  */
@@ -26,6 +26,11 @@ class Userrenewlog extends Backend
     {
         $tableName = $this->getRecordYearTableName('Task_Detail_4_20');
         if ($this->request->isAjax()) {
+            $filter = $this->request->get("filter", '');
+            $filter = json_decode($filter, TRUE);
+            if(empty($filter['d.tit']) ){
+                $this->error('请设置域名搜索条件域名后查询数据');
+            }
             list($where, $sort, $order, $offset, $limit,$spre,$special_condition,$year) = $this->buildparams();
             $year = empty($year) ? '_'.$tableName[0] : '_'.$year;
             $condition = '';
@@ -39,14 +44,13 @@ class Userrenewlog extends Backend
             }
 
             $total = $this->model->table(PREFIX.'Task_record')->alias('r')->join(PREFIX.'Task_Detail_4'.$year.' d','r.id = d.taskid','left')->join('domain_user u','r.userid=u.id','left')
-                ->where($where)->where(' d .TaskStatusCode = 2 and r.tasktype = 4 '.$condition)
+                ->where($where)->where('r.tasktype = 4 '.$condition)
                 ->count();
             $list = $this->model->table(PREFIX.'Task_record')->alias('r')->join(PREFIX.'Task_Detail_4'.$year.' d','r.id = d.taskid','left')->join('domain_user u','r.userid=u.id','left')
-                ->field('r.id,r.uip,r.createtime,uid,d.tit,d.money,d.api_id')
-                ->where($where)->where(' d .TaskStatusCode = 2 and r.tasktype = 4 '.$condition)
+                ->field('r.id,r.status,d.ErrorMsg,r.uip,r.createtime,uid,d.tit,d.money,d.api_id,d.TaskStatusCode,d.CreateTime')
+                ->where($where)->where('r.tasktype = 4 '.$condition)
                 ->order($sort,$order)->limit($offset, $limit)
                 ->select();
-
             $arr = [];
             // 单价总金额
             //根据条件统计总金额
@@ -58,6 +62,7 @@ class Userrenewlog extends Backend
 
             $res = $this->model->query($conm);
             $apis = $this->getApis(-1);
+            $fun = Fun::ini();
             foreach($list as $k => $v){
                 $arr[$k]['u.uid'] =$v['uid'];
                 if(empty($v['uip'])){
@@ -65,13 +70,23 @@ class Userrenewlog extends Backend
                 }else{
                     $arr[$k]['r.uip'] = $v['uip'];
                 }
+                $arr[$k]['d.CreateTime'] = $v['CreateTime'];
+                $arr[$k]['d.tit'] = $v['tit'];
+                $arr[$k]['r.status'] = $fun->getStatus($v['status'],['<span style="color: red;">执行中</span>','<span style="color: green;">已完成</span>']);
+                $arr[$k]['d.ErrorMsg'] = $v['ErrorMsg'];
+                $arr[$k]['d.CreateTime'] = $v['CreateTime'];
                 $arr[$k]['d.money'] = sprintf('%.2f',$v['money']);
                 $arr[$k]['r.createtime'] = $v['createtime'];
                 $arr[$k]['zje'] =$res[0]['n'];
                 $arr[$k]['d.tit'] =$v['tit'];
-                $arr[$k]['api_id'] = $apis[$v['api_id']]['tit'];
-                $arr[$k]['special_condition'] = $apis[$v['api_id']]['regname'];
+                $arr[$k]['id'] =$v['id'];
+                $arr[$k]['d.TaskStatusCode'] = $v['TaskStatusCode'];
+                if (!empty($v['api_id'])){
+                    $arr[$k]['api_id'] = $apis[$v['api_id']]['tit'];
+                    $arr[$k]['special_condition'] = $apis[$v['api_id']]['regname'];
+                }
             }
+
             $result = array("total" => $total, "rows" => $arr);
             return json($result);
         }
--- public/assets/js/backend/vipmanage/recode/userrenewlog.js
+++ public/assets/js/backend/vipmanage/recode/userrenewlog.js
@@ -22,11 +22,11 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 pageList: [10, 25, 50,100,200, 'All'],
                 columns: [
                     [
+                        {field: 'id', title: '主任务ID',operate: false},
+                        {field: 'd.tit', title: '域名',operate: 'TEXT',},
                         {field: 'u.uid', title: '用户名',footerFormatter: function (data) {
                                 return '统计：';//在第一列开头写上总计、统计之类
                             }},
-                        {field: 'r.createtime', title: '任务提交时间',operate: 'INT',sortable:true, addclass: 'datetimerange', formatter: Table.api.formatter.datetime,defaultValue:getTimeFrame()},
-                        {field: 'd.tit', title: '域名',operate: 'TEXT',},
                         {field: 'd.money', title: '单价',operate: false,sortable:true,footerFormatter: function (data) {
                                 var field = 'zje';
                                 var total_sum = data.reduce(function (sum, row) {
@@ -38,6 +38,21 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'special_condition', title: '注册商', searchList: $.getJSON('category/getcategory?type=api&xz=parent') },
                         {field: 'group', title: '后缀', visible:false, searchList: $.getJSON('domain/manage/getDomainHz'),},
                         {field: 'api_id', title: '接口商', searchList: $.getJSON('webconfig/regapi/getRegisterUserName'),},
+                        {field: 'r.status', title: '执行进度', formatter: Table.api.formatter.status,notit:true, searchList: {0:"执行中",1:"执行成功"}},
+                        {field: 'r.createtime', title: '任务提交时间',operate: 'INT',sortable:true, addclass: 'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'd.TaskStatusCode', title: '状态',searchList:{0:'等待执行',1:'执行中',2:'执行成功',3:'执行失败'},formatter:function(value){
+                            console.log(value)
+                            if (value == 0) {
+                                    return '等待执行';
+                                }else if(value == 1){
+                                    return '执行中';
+                                }else if(value == 2){
+                                    return '执行成功';
+                                }else if(value == 3){
+                                    return '执行失败';
+                                }
+                            }},
+                        {field: 'd.CreateTime', title: '任务结束时间', addclass: 'datetimerange', formatter: Table.api.formatter.datetime},
                         {field: 'r.uip', title: '注册IP',formatter:Table.api.formatter.alink,url:'http://www.baidu.com/s',fieldvaleu:'r.uip',fieldname:'wd',tit:'Ip归属地查询',},
                     ]
                 ],
