2b5861a62b92324ef9ff828f962112af3d40d60e
luzhizhuang <hm123123>
2021-06-18 11:51:43 +0800

    优化问题反馈

--- application/admin/controller/orderfx/Feedback.php
+++ application/admin/controller/orderfx/Feedback.php
@@ -32,11 +32,16 @@ class Feedback extends Backend
     {
         if ($this->request->isAjax())
         {
-            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
-            $total = $this->db->where($where)->count();
+            list($where, $sort, $order, $offset, $limit,$group) = $this->buildparams();
+
+            $def = '';
+            if($group){
+                $def = ' id = '.str_replace($group,'WD','');
+            }
+            $total = $this->db->where($def)->where($where)->count();
             $list = $this->db
-                    ->field('id,uid,uqq,type,nickname,desc,create_time,money,cnstatus,status')
-                    ->where($where)
+                    ->field('id,uid,uqq,type,nickname,desc,create_time,money,cnstatus,status,ex_status,ex_remark')
+                    ->where($def)->where($where)
                     ->order($sort, $order)
                     ->limit($offset, $limit)
                     ->select();
@@ -44,9 +49,17 @@ class Feedback extends Backend
             foreach($list as $k=>$v){
                 $list[$k]['type'] = $fun->getStatus($v['type'],['功能改进','在线提问','其他']);
                 $list[$k]['status'] = $fun->getStatus($v['status'],['<span style="color:red">未阅读</span>','<span style="color:green">已阅读</span>']);
+                $list[$k]['eex_status'] = $v['ex_status'];
+                $list[$k]['ex_status'] = $fun->getStatus($v['ex_status'],['<span style="color:red">待处理</span>','<span style="color:green">已处理</span>','<span style="color:orange">不处理</span>']);
                 $list[$k]['cnstatus'] = $fun->getStatus($v['cnstatus'],['<span style="color:red">未采纳</span>','<span style="color:green">已采纳</span>']);
+                $list[$k]['group'] = 'WD'.$v['id'];
                 if($v['cnstatus'] == 0){
                   $list[$k]['money'] = "--";
+                }
+                if($v['ex_remark']){
+                    $list[$k]['ex_remark'] = '<span class="show_value" style="cursor:pointer;color:#3c8dbc;" onclick="showRemark(\''.$v['ex_remark'].'\')" >查看</span>';
+                }else{
+                    $list[$k]['ex_remark'] = '--';
                 }
                 // $list[$k]['check'] = '<a href="orderfx/order/feedbackedit?ids='.$v["id"].'" class="btn btn-xs btn-success btn-editone btn-dialog" title="查看"><i class="fa fa-pencil"></i></a>';
             }
@@ -74,4 +87,23 @@ class Feedback extends Backend
         return $this->view->fetch();
     }
 
+    public function modi(){
+        if($this->request->isAjax()){
+            $param = $this->request->param();
+            if(empty($param['id']) || empty($param['status'])){
+                $this->error('缺少重要参数');
+            }
+            if(!in_array($param['status'],[1,2])){
+                $this->error('参数不在合法范围');
+            }
+            $flag = $this->db->where(['id' => $param['id'],'status' => 0])->count();
+            if(empty($flag)){
+                $this->error('记录不存在或状态已发生改变');
+            }
+            $remark = empty($param['remark']) ? '' : $param['remark'];
+            $this->db->where('id',$param['id'])->update(['ex_status' => $param['status'],'ex_remark' => $remark]);
+            $this->success('操作成功');
+
+        }
+    }
 }
--- public/assets/js/backend/orderfx/feedback.js
+++ public/assets/js/backend/orderfx/feedback.js
@@ -22,6 +22,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 columns: [
                     [
                         { checkbox: true,},
+                        { field: 'group', title: '编号',},
                         { field: 'uid', title: '用户名',sortable: true,},
                         { field: 'uqq', title: 'QQ号', sortable: true,},
                         { field: 'nickname', title: '昵称',sortable: true,},
@@ -30,8 +31,52 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         { field: 'type', title: '反馈类型',searchList: {0:'功能改进',1:'在线提问',2:'其他'},},
                         { field: 'status', title: '阅读状态',searchList:{0:"未阅读",1:"已阅读"}},
                         { field: 'cnstatus', title: '采纳状态',searchList:{0:"未采纳",1:"已采纳"}},
+                        { field: 'ex_status', title: '处理状态',searchList:{0:"待处理",1:"已处理",2:'不处理'}},
+                        { field: 'ex_remark', title: '不处理原因',},
                         { field: 'create_time', title: '提交时间',operate: 'RANGE',addclass: 'datetimerange',formatter:Table.api.formatter.datetime,},
-                        {field: 'operate', title: __('Operate'), table: table,events: Table.api.events.operate,formatter: Table.api.formatter.operate,},
+                        { field: 'operate', title: __('Operate'),
+                            width:150,
+                            table: table,events: Table.api.events.operate,
+                            formatter: Table.api.formatter.operate,
+                            buttons: [{
+                                name: '已处理',
+                                text: '已处理',
+                                title: '已处理',
+                                classname: 'btn btn-xs btn-warning btn-magic btn-ajax',
+                                confirm:'确定要处理吗？',
+                                url:function(res){
+                                    return '/admin/orderfx/feedback/modi?id='+res.id+'&status=1';
+                                },
+                                visible:function(res){
+                                    if(res.eex_status == 0){
+                                        return true;
+                                    }
+                                    return false;
+                                },
+                                success: function (data,ret) {
+                                    if(ret.code==1){
+                                        $('.btn-refresh').click();
+                                    }else{
+                                        layer.msg(ret.msg);
+                                    }
+                                    return false;
+                                },
+                            },{
+                                name: '不处理',
+                                text: '不处理',
+                                title: '不处理',
+                                classname: 'btn btn-xs btn-danger btn-magic',
+                                extend:function(res){
+                                    return 'onclick="modi('+res.id+')"';
+                                },
+                                visible:function(res){
+                                    if(res.eex_status == 0){
+                                        return true;
+                                    }
+                                    return false;
+                                }
+                            }],
+                        },
                     ]
                 ],
             });
@@ -59,4 +104,24 @@ function zhanshi(id){
     }else{
        $('#money').show();
     }
+}
+
+/**
+ * 不处理
+ * @param id
+ */
+function modi(id){
+    layer.prompt({title: '请输入备注', formType: 2}, function(text, index){
+        layer.close(index);
+        layer.load(0);
+        $.post('/admin/orderfx/feedback/modi',{id:id,remark:text,status:2},function(res){
+            layer.closeAll('loading');
+            if(res.code==1){
+                $('.btn-refresh').click();
+            }else{
+                layer.msg(res.msg);
+            }
+        },'json');
+
+    });
 }
